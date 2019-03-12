4ed7712aaddc517d928ae6d8d3869153be0f62d9
Omer Hefets <hefets.omer@gmail.com>
2019-03-12 18:21:24 +0200

    write a few more tests

--- test_grid_api.py
+++ test_grid_api.py
@@ -21,13 +21,34 @@ g = Grid(3)
     # X 3rd column
     ([-1, 0, 1, -1, -1, 1, -1, 0, 1], "x_player"),
 ])
-def test_winner(test_input, expected):
+def test_find_a_winner(test_input, expected):
     g.set_grid(test_input)
     assert g.find_a_winner() == expected
 
 
-def test_choose_location(monkeypatch):
-    g.set_grid([-1, -1, -1, -1, 1, -1, -1, -1, -1])
-    monkeypatch.setattr('builtins.input', lambda x: 4)
-    assert g.choose_location('x', 'human')
+@pytest.mark.parametrize("test_input,expected", [
+    (0, (0, 0)),
+    (3, (1, 0)),
+    (5, (1, 2)),
+    (6, (2, 0)),
+    (8, (2, 2))
+])
+def test_change1d_to_2d(test_input, expected):
+    assert g.change1d_to_2d(test_input) == expected
+
+
+# test_input = [grid, user_location]
+@pytest.mark.parametrize("test_input,expected", [
+    (([0, 1, 0, 1, 0, 1, 0, 1, -1], 8), True),
+    (([0, 0, 0, -1, -1, -1, 1, 1, -1], 0), False),
+    (([-1, -1, 0, -1, 0, 1, -1, 1, 0], 0), True),
+    (([1, 1, 1, -1, 1, 0, 0, 0, -1], 3), True),
+    (([1, 0, 0, 1, 1, 1, 0, 0, -1], 6), False),
+    (([-1, -1, -1, -1, -1, -1, -1, -1, -1], 1), True),
+    (([-1, 0, 1, -1, -1, 1, -1, 0, 1], 1), False)
+])
+def test_choose_location(test_input, expected, monkeypatch):
+    g.set_grid(test_input[0])
+    monkeypatch.setattr('builtins.input', lambda x: test_input[1])
+    assert g.choose_location('x', 'human') == expected
 
