a94796f051b455e6c228b05acac2676e42fad2b1
Omer Hefets <hefets.omer@gmail.com>
2019-03-23 22:46:37 +0200

    basic changes to nn api

--- nn_api.py
+++ nn_api.py
@@ -3,44 +3,46 @@ import numpy as np
 
 
 class NeuralNet:
-    final_weights = {}
+    weights = {}
+    bias = {}
 
     def __init__(self, layers):
         self.layers = layers
+        self.init_random_weights()
 
     @staticmethod
-    def f(x):
+    def sigma(x):
         return 1 / (1 + np.exp(-x))
 
-    def init_random_weights(self):
-        for i in range(len(self.layers)-1):
-            # each matrix is the size of [l_1][l_0 + 1], (+1 because of the bias)
-            random_weights = np.random.rand(self.layers[i+1], self.layers[i]+1)
-            random_weights -= 0.5
-            random_weights *= 2
-            self.final_weights[i] = random_weights
+    def sigma_der(self, x):
+        return self.sigma(x) * (1 - self.sigma(x))
 
-    def vectorized_ff(self, x_vector, add_bias_to_vector):
-        weights = self.final_weights
-        vector = x_vector
+    def init_random_weights(self):
+        for i in range(len(self.layers) - 1):
+            # each weights is the size of [l_1][l_0]
+            random_weights = np.random.rand(self.layers[i + 1], self.layers[i])
+            random_weights = (random_weights - 0.5) * 2
+            self.weights[i] = random_weights
+            # each bias is the size of [l_1][1]
+            random_bias = np.random.rand(self.layers[i + 1], 1)
+            random_bias = (random_bias - 0.5) * 2
+            self.bias[i] = random_bias
+
+    def vectorized_ff(self, x_vector):
         z = {}
         h = {}
-        if add_bias_to_vector:
-            vector = np.vstack((1, vector))
-        for i in range(len(self.layers)-1):
-            if not i == 0:
-                vector = np.vstack((1, h[i]))
-            z[i] = np.dot(weights[i], vector)
-            h[i+1] = self.f(z[i])
-        return h[len(self.layers)-1]
+        vector = x_vector
+        for layer in range(len(self.layers) - 1):
+            if layer > 0:
+                ##
+            z[layer] = np.dot(self.weights[layer], vector) + self.bias[layer]
+            h[layer + 1] = self.sigma(z[layer])
+        return h, z
 
 
 # init net
 nn_layers = [500, 100, 9]
 nn = NeuralNet(nn_layers)
-nn.init_random_weights()
-
 
 # init random input vector
 random_vector = np.random.rand(500, 1)
-print(nn.vectorized_ff(random_vector, add_bias_to_vector=True))
--- tests/test_nn_api.py
+++ tests/test_nn_api.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 @pytest.mark.parametrize("test_input,expected", [
-    ([500, 100, 9], ((100, 501), (9, 101))),
-    ([30, 30, 1], ((30, 31), (1, 31))),
-    ([10, 15, 30], ((15, 11), (30, 16)))
+    ([500, 100, 9], ((100, 500), (9, 100))),
+    ([30, 30, 1], ((30, 30), (1, 30))),
+    ([10, 15, 30], ((15, 10), (30, 15)))
 
 ])
 def test_init_random_weights(test_input, expected):
     nn = NeuralNet(test_input)
     nn.init_random_weights()
     for i in range(len(test_input)-1):
-        weights = nn.final_weights[i]
+        weights = nn.weights[i]
         assert np.shape(weights) == expected[i]
